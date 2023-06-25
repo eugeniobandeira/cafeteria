14847b7987e9dbe6a234c950b14b3fcd60f6ef16
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2023-06-25 11:49:50 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// const inputCheck = document.querySelector("#modo-noturno")
-// const elemento = document.querySelector('body')
 
-// alterarTema.addEventListener('click', () => {
-//     const modo = inputCheck.checked ? "dark" : "light"
-//     elemento.setAttribute("data-bs-theme", modo)
-// })
 const inputCheck = document.querySelector('#modo-noturno')
 const elemento = document.querySelector('body')
 
